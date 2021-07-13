22a8c2811d615d37b40396f1d663d1501404fbe7
Susheel Varma <susheel.varma@gmail.com>
2021-07-13 14:33:32 +0100

    major fixes

--- scripts/yamlify.py
+++ scripts/yamlify.py
@@ -39,6 +39,9 @@ def extract_sections(issue_data):
 def format_yaml(sections):
     category = CATGEORIES[sections['main-category'][0]]
     filename = "/".join([category, sections['name'][0]]) + ".yaml"
+    categories = sections['categories'][0]
+    categories = sections['main-category'] + categories.split(',')
+    categories = list(set(categories))
     data = {
         'name': sections['name'][0],
         'url': sections['url'][0],
